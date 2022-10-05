591b4bcf95fb441815024ffbe8f59d1c9773f615
goodylove474 <goodyc474@gmail.com>
2022-10-05 12:55:08 +0100

    first commit

---
+++ json.js
@@ -0,0 +1,53 @@
+const musicBank = [
+  {
+    Artist: "2Face Idibia",
+    songTitle: "African Queen",
+    imggUrl: "./music/2face/2face African QUEEN.png",
+    songUrl: "./music/2face/2Face Idibia - African Queen.mp3",
+  },
+  {
+    Artist: "6Lack",
+    songTitle: "Pretty little fears",
+    imggUrl: "./music/6lack/pretyy little fears.png",
+    songUrl: "./music/6lack/6LACK_-_Pretty_Little_Fears_ft_J_Cole.mp3",
+  },
+  {
+    Artist: "Ab-on deck",
+    songTitle: "Cadabra-on-Deck",
+    imggUrl: "./music/Ab - on deck/Abra-Cadabra-On-Deck.jpg",
+    songUrl:
+      "./music/Ab - on deck/Abra_Cadabra_Ft_RV_Kush_Double_Lz_Bandokay_Lowkey_Dezzie_-_On_Deck_Remix_ - Copy.mp3",
+  },
+  {
+    Artist: "Burna boy",
+    songTitle: "Twice as Tall",
+    imggUrl:
+      "./music/Burna boy - Twice as Tall/Twice_as_Tall_Burna_Boy_Album.jpg",
+    songUrl:
+      "./music/Burna boy - Twice as Tall/01-Burna-Boy-Level-Up-Twice-As-Tall-ft-Youssou-N-Dour-360media.com_.ng_.mp3",
+  },
+  {
+    Artist: "Omah lay",
+    songTitle: "Godly",
+    imggUrl: "./music/Omah lay - Godly/Omah-Lay-What-Have-We-Done-EP.jpg",
+    songUrl: "./music/Omah lay - Godly/03 Omah Lay - Godly (NetNaija.com).mp3",
+  },
+  {
+    Artist: "Ruger",
+    songTitle: "Bounce",
+    imggUrl: "music/Ruger - bounce/Ruger-Bounce.jpg",
+    songUrl: "music/Ruger - bounce/05-Ruger-Bounce-(JustNaija.com).mp3",
+  },
+  {
+    Artist: "Koffe",
+    songTitle: "Toast",
+    imggUrl: "music/koffe - toast/toast.jpg",
+    songUrl: "./music/koffe - toast/Koffee - Toast (Official Video).mp3",
+  },
+  {
+    Artist: "Roddy",
+    songTitle: "Die young",
+    imggUrl: "./music/roddy/Roddy_Ricch_-_Die_Young.jpg",
+    songUrl: "./music/roddy/04. Die Young.mp3",
+  },
+];
